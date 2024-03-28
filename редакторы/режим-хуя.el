85b8d639725e15bb92cf9a3c8131470fb22b3fa9
rexim <reximkut@gmail.com>
2024-03-28 20:08:33 +0700

    Улучшить подсветку литералов хуя для емакса

--- редакторы/режим-хуя.el
+++ редакторы/режим-хуя.el
@@ -7,9 +7,9 @@
 	(modify-syntax-entry ?\n "> b")
 
     ;; Строковые литералы
-    (modify-syntax-entry ?' "\"")
     (modify-syntax-entry ?« "(»")
     (modify-syntax-entry ?» ")«")
+    (modify-syntax-entry ?\" ".")
 
     (syntax-table))
   "Синтаксическая таблица для `режим-хуя'.")
@@ -21,10 +21,39 @@
       "структ" "союз" "как" "вкл" "внешняя" "библ" "или"
       "и" "истина" "ложь" "лбс" "пбс" "ост")))
 
+(defun строковый-литерал-хуя (придел)
+  (while (and (< (point) придел)
+              (not (or (eq (char-after) ?«) (eq (char-after) ?\"))))
+    (forward-char))
+  (cond
+   ((eq (char-after) ?«)
+    (let ((начало (point)))
+      (forward-char)
+      (let ((вложенность 1))
+        (while (and (< (point) придел) (> вложенность 0))
+          (cond
+           ;; СДЕЛАТЬ: поддержка экранирования внутри «ёлочных» литералов
+           ((eq (char-after) ?«) (incf вложенность))
+           ((eq (char-after) ?») (decf вложенность)))
+          (forward-char)))
+      (set-match-data (list начало (point)) t)
+      (point)))
+   ((eq (char-after) ?\")
+    (let ((начало (point)))
+      (forward-char)
+      (let ((кончили nil))
+        (while (and (< (point) придел) (not кончили))
+          ;; СДЕЛАТЬ: поддержка экранирования внутри "лапочных" литералов
+          (when (eq (char-after) ?\")
+            (setq кончили t))
+          (forward-char)))
+      (set-match-data (list начало (point)) t)
+      (point)))))
+
 ;; СДЕЛАТЬ: подсветка многострочных «ёлочных» литералов.
-;; СДЕЛАТЬ: экранированные «ёлочки» внутри «ёлочных» литералов.
+;; Она почему-то не работает из коробки
 (defconst подсветка-хуя
-  `(("«.*?»" . font-lock-string-face)
+  `((строковый-литерал-хуя . font-lock-string-face)
     (,(regexp-opt ключевые-слова-хуя 'symbols) . font-lock-keyword-face)))
 
 ;;;###autoload
